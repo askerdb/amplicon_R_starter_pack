030c57ccbc6430a8304ed2638de169e0a0dfc904
unknown <chs962@BA09092.science.domain>
2014-05-24 19:16:25 +0200

    Fixed filename bug

--- load_data.R
+++ load_data.R
@@ -3,7 +3,7 @@ source("import_conversion_common.R")
 #If the data was not denovo clustered load the full gg_13 tree. This is pretty big.
 #TODO: find some automatic way of validating that OTUs were actually picked against gg13
 if (file.exists("rep_set_midpoint.tre")){
-  all_samples load_phyloseq("mapping.txt", "otu_table.biom", "rep_set_midpoint.tre")
+  all_samples load_phyloseq("mapping.txt", "otu_table.biom", "rep_set.tre")
 }
 else{
   #Tree taken from https://groups.google.com/forum/#!msg/qiime-forum/v-TfjP20uws/uvvySwyHPAsJ
